327bc2b841affa450724f5000f0643e2f4e49d84
durtghost <drutghost@gmail.com>
2024-10-09 20:04:48 +0100

    revisao da aula da claculadora

---
+++ durt porgt2/calculadora.py
@@ -0,0 +1,8 @@
+print("bem vindo a calculadora do ghost")
+
+primeironumero =int(input("\ndigete o primeiro numero "))
+sengundonumero =int(input("\ndigete o sengundo numero"))
+
+calculo = primeironumero + sengundonumero
+print(f"A soma dos valores é igual a: {calculo}")
+x\
