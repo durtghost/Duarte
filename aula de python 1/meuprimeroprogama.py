d08223fe5f76db55fb225815bf22b7f469688357
durtghost <drutghost@gmail.com>
2024-10-09 19:15:03 +0100

    Initial commit

---
+++ aula de python 1/meuprimeroprogama.py
@@ -0,0 +1,22 @@
+#Primero se da o nome para a vareival
+#Sengundo: coloca o sinal de igual receba
+# treciro: da alguma coisa para a variavel
+from operator import truediv
+
+nome = "durt" # esta variavel guardou letras
+vareaveldecime =  95.37 # variavel que guadar numero com virgula
+vareavelveroufals = True # o T e o F
+
+
+#varivels que recebem enter ( ou seja, vao ser declarada durante a porgmam
+
+variavelnome = input("Digite o nome ")
+
+numero1 = int(input("digite o numero " ))
+numero2 = float(input("digite o decimal "))
+varvouF = bool(input("digite True e False \n "))
+
+print(f"A varaivel nome guadada  {variavelnome} ")
+print(f"A varaivel 1 guadada {numero1}")
+print(f"A varaivel 2 guadada {numero2}")
+print(f"A varaivel V ou F guadada {varvouF}")
